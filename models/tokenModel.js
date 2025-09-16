9bd701947857793632aea00d422ca1def9d81a99
hanzla59 <hanzla.bscsses21@iba-suk.edu.pk>
2025-09-16 17:20:58 +0500

    implement transaction in auth (either complete or reject) and check input for api to protect from sql

--- models/tokenModel.js
+++ models/tokenModel.js
@@ -1,32 +1,32 @@
 const pool = require('../config/db');
 
 const Token = {
-  async create(userId, token) {
-    const result = await pool.query(
+  async create(userId, token, client = pool) {
+    const result = await client.query(
       `INSERT INTO refresh_tokens (user_id, token) VALUES ($1, $2) RETURNING *`,
       [userId, token]
     );
     return result.rows[0];
   },
 
-    async updateToken (id, newToken){
-    const result = await pool.query(
+    async updateToken (id, newToken, client = pool){
+    const result = await client.query(
       `UPDATE refresh_tokens SET token = $1 WHERE id = $2 RETURNING *`,
       [newToken, id]
     );
     return result.rows[0];
   },
 
-  async find(token) {
-    const result = await pool.query(
+  async find(token, client = pool) {
+    const result = await client.query(
       `SELECT * FROM refresh_tokens WHERE token = $1`,
       [token]
     );
     return result.rows[0];
   },
 
-  async delete(token) {
-    await pool.query(`DELETE FROM refresh_tokens WHERE token = $1`, [token]);
+  async delete(token, client = pool) {
+    await client.query(`DELETE FROM refresh_tokens WHERE token = $1`, [token]);
   }
   
 };
